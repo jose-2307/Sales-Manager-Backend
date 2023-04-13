e0573bdcbf32d3a9a3067d3a65516578f9d65a21
joselatam <joseluisflores2307@gmail.com>
2023-04-13 17:51:13 -0400

    feat: add router

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+
+//routers específicos
+const roleRouter = require("./role.router");
+
+const routerApi = (app) => {
+  const router = express.Router();
+  app.use("/api/v1",router);
+  router.use("/roles",roleRouter);
+
+}
+
+module.exports = routerApi;
